f5a448a65a877b4d3017cfd012214d2b133c25e4
Alberto GAray <yarag44@hotmail.com>
2022-08-14 13:09:06 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,274 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React,{ useState, useRef, useEffect, useMemo, useCallback}  from 'react';
+import {Button,Card,Row,Col} from 'react-bootstrap';
+
+//import logo from './logo.svg';
 import './App.css';
+//import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+
+import * as constants from './constants';
+
+//import Parse from 'parse';
+
+const animatedComponents = makeAnimated();
+
 
 function App() {
+
+  const gridRef = useRef(); // Optional - for accessing Grid's API
+  const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+
+  const [selectedResidential,setSelectedResidential] = useState({ value :0,label: 'Select' });
+
+
+    // Each Column Definition results in one Column.
+  const [columnDefs, setColumnDefs] = useState([
+    {field: 'residentname', filter: true},
+    {field: 'residential', filter: true},
+    {field: 'house', filter: true},
+    {field: 'year', filter: true},
+    {field: 'Jan'},
+    {field: 'Feb'},
+    {field: 'Mar'},
+    {field: 'Apr'},
+    {field: 'May'},
+    {field: 'Jun'},
+    {field: 'Jul'},
+    {field: 'Aug'},
+    {field: 'Sep'},
+    {field: 'Oct'},
+    {field: 'Nov'},
+    {field: 'Dec'},
+    {field: 'yearamount'},
+    
+  ]);
+
+// DefaultColDef sets props common to all Columns
+const defaultColDef = useMemo( ()=> ({
+  sortable: true
+}));
+
+ // Example of consuming Grid Event
+const cellClickedListener = useCallback( event => {
+  console.log('cellClicked', event);
+}, []);
+
+// Example load data from sever
+useEffect(() => {
+  // fetch('http://localhost:3000/report')
+  // .then(result => result.json())
+  // .then(rowData => setRowData(rowData))
+
+    newGrid();
+
+
+
+}, []);
+
+const newGrid = async () => {
+
+
+  //console.log(constants.path_url + 'report?Residential=' + selectedResidential.value.toString());
+
+  let result = await axios.get(constants.path_url + 'report?Residential=' + selectedResidential.value.toString() )
+
+  //console.log(JSON.parse(JSON.stringify(result)).data);
+
+  setRowData(JSON.parse(JSON.stringify(result)).data);
+
+}
+
+
+
+// Example using Grid's API
+const buttonListener = useCallback( e => {
+  gridRef.current.api.deselectAll();
+
+  //console.log(rowData);
+
+
+}, []);
+
+  //const [dataReport,setDataReport] = useState([]);
+
+ 
+
+  //  useEffect(() => {
+
+  //    //console.log('Entro');
+
+  //    showReport();
+
+  //  },[])
+
+
+  //  const showReport = async () => {
+      
+  //   let result = await axios.get('http://localhost:3000/report')
+ 
+  //   //console.log(JSON.parse(JSON.stringify(result)));
+ 
+  //   setDataReport(JSON.parse(JSON.stringify(result)));
+    
+  //  }
+
+  const options = [
+    { value: '1', label: 'VISTA REAL' },
+  ]
+
+  const methodOnChange = (item) => {
+
+    if(!item) {setSelectedResidential({ value :0,label: 'Select' }); return;} 
+
+    //gridRef.current.api.deselectAll();
+    setSelectedResidential(item)
+   
+
+  }
+
+
+  useEffect(() => {
+
+    console.log('Entro actualizar');
+    newGrid();
+
+  },[selectedResidential])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <>
+
+    <Row className={'mx-auto'} style={{ 'marginTop' : 15 }}>
+
+      <Col>
+      
+       <Card >
+      <Card.Header><h3>Pagos por Residencial</h3></Card.Header>
+      <Card.Body>
+        {/* <Card.Title>Pagos por Residencial</Card.Title> */}
+          <Row className={'mx-auto'}>
+            <Col>
+            
+              <Select
+                closeMenuOnSelect={true}
+                isClearable
+                //components={animatedComponents}
+                //defaultValue={[colourOptions[4], colourOptions[5]]}
+                placeholder={'SELECCIONA RESIDENTIAL ....'}
+                onChange={(e) => { methodOnChange(e) } }
+                //isMulti
+                options={options}
+              />
+            
+            </Col>
+
+          </Row>
+
+          <Row className={'mx-auto'}>
+            <Col className={'ag-theme-alpine'} style={{width: '100%', height: '600px'}}>
+            
+            <AgGridReact
+              ref={gridRef} // Ref for accessing Grid's API
+
+              rowData={rowData} // Row Data for Rows
+
+              columnDefs={columnDefs} // Column Defs for Columns
+              defaultColDef={defaultColDef} // Default Column Properties
+
+              animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+              rowSelection='multiple' // Options - allows click selection of rows
+
+              onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+              /> 
+
+            </Col>
+          </Row>
+
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+      </Card>
+      
+      </Col>
+
+
+    </Row>
+
+
+   
+
+
+
+
+
+    {/* <Button variant="primary">Primary</Button>
+    <div style={{ width: '1000px' }}>
+
+    <Select
+      closeMenuOnSelect={true}
+      isClearable
+      //components={animatedComponents}
+      //defaultValue={[colourOptions[4], colourOptions[5]]}
+      placeholder={'RESIDENTIAL ....'}
+      onChange={(e) => { methodOnChange(e) } }
+      //isMulti
+      options={options}
+    />
+
     </div>
+
+
+    <div className="ag-theme-alpine" style={{width: '1000px', height: '600px'}}>
+      
+    <AgGridReact
+    ref={gridRef} // Ref for accessing Grid's API
+
+    rowData={rowData} // Row Data for Rows
+
+    columnDefs={columnDefs} // Column Defs for Columns
+    defaultColDef={defaultColDef} // Default Column Properties
+
+    animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+    rowSelection='multiple' // Options - allows click selection of rows
+
+    onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+    />
+   
+   
+   </div> */}
+
+    
+    </>
+    
+
+
+//     <div>
+      
+//       <h1>Test Alberto Garay</h1><br></br>
+
+// {/* {console.log(dataReport.data[0].residential)} */}
+//       {/* {dataReport.data && dataReport.data.map(item => {
+//         {<h3>Residential</h3>}
+
+      
+
+//       })}  */}
+//           <button onClick={buttonListener}>Push Me</button>
+
+//           {console.log(rowData)}
+
+
+
+
+//     </div>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+
+export const path_url = "https://prod-report-pays-api.herokuapp.com/";
+
