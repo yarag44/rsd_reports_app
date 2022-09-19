85707c337f2a58d2de8824b1773ec390a3bbc3db
Alberto GAray <yarag44@hotmail.com>
2022-09-18 19:37:27 -0700

    Config 2 NPM Install

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React,{ useState, useRef, useEffect, useMemo, useCallback}  from 'react';
+import React,{ useState, useRef, useEffect, useMemo, useCallback, startTransition}  from 'react';
 import {Card,Row,Col} from 'react-bootstrap';
 //import logo from './logo.svg';
 import './App.css';
